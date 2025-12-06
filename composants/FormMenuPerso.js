96e8e2fa46cca71594bf8a7242032f295ab429ab
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:34:38 +0100

    Update FormMenuPerso.js

--- composants/FormMenuPerso.js
+++ composants/FormMenuPerso.js
@@ -1,1 +1,12 @@
-export default function FormMenuPerso(){ return ( <form style={{display:'flex', flexDirection:'column', maxWidth:'400px', margin:'auto'}}> Starter: Main: Dessert: Drink: Notes: <button type='submit' style={{marginTop:'1rem'}}>Submit ) }
+export default function FormMenuPerso(){
+  return (
+    <form style={{display:'flex', flexDirection:'column', maxWidth:'400px', margin:'auto'}}>
+      <label>Starter:</label><input type='text' />
+      <label>Main:</label><input type='text' />
+      <label>Dessert:</label><input type='text' />
+      <label>Drink:</label><input type='text' />
+      <label>Notes:</label><textarea placeholder='Allergies or preferences'></textarea>
+      <button type='submit' style={{marginTop:'1rem'}}>Submit</button>
+    </form>
+  )
+}
