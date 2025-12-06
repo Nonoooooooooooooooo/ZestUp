62689653b063e2ad9d94308c1d845c7976ec83a2
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:33:13 +0100

    Update Navbar.js

--- composants/Navbar.js
+++ composants/Navbar.js
@@ -1,1 +1,15 @@
-import Link from 'next/link'; export default function Navbar(){ return ( <nav style={{display:'flex', justifyContent:'space-between', padding:'1rem 2rem', backgroundColor:'#FF7F50', color:'white'}}> ZestUp <div style={{display:'flex', gap:'1rem'}}> Home Menus Reservation Order Contact ) }
+import Link from 'next/link';
+export default function Navbar(){
+  return (
+    <nav style={{display:'flex', justifyContent:'space-between', padding:'1rem 2rem', backgroundColor:'#FF7F50', color:'white'}}>
+      <h2>ZestUp</h2>
+      <div style={{display:'flex', gap:'1rem'}}>
+        <Link href='/'>Home</Link>
+        <Link href='/menus'>Menus</Link>
+        <Link href='/reservation'>Reservation</Link>
+        <Link href='/commande'>Order</Link>
+        <Link href='/contact'>Contact</Link>
+      </div>
+    </nav>
+  )
+}
