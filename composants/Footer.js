b0a3fa0d3c0674e879abbdbef250ecc01901a313
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:46:01 +0100

    Update Footer.js

--- composants/Footer.js
+++ composants/Footer.js
@@ -1,6 +1,13 @@
 export default function Footer(){
   return (
-    <footer style={{textAlign:'center', padding:'1rem', backgroundColor:'#FF7F50', color:'white', marginTop:'2rem'}}>
+    <footer style={{
+      textAlign:'center',
+      padding:'1.5rem',
+      backgroundColor:'#FF7F50',
+      color:'white',
+      marginTop:'2rem',
+      boxShadow:'0 -4px 6px rgba(0,0,0,0.1)'
+    }}>
       <p>© 2025 ZestUp. All rights reserved.</p>
     </footer>
   )
