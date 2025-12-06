34170bb75e7f0a78cb7955c4ca40b2c448ec3eac
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:08:32 +0100

    Update contact.js

--- Pages/contact.js
+++ Pages/contact.js
@@ -1,1 +1,3 @@
+import Navbar from '../composants/Navbar'; import Footer from '../composants/Footer';
 
+export default function Contact() { return ( <> <main style={{padding:'2rem'}}> Contact Us <form style={{display:'flex', flexDirection:'column', maxWidth:'400px', margin:'auto'}}> Name: Email: Message: <button type='submit' style={{marginTop:'1rem'}}>Send </> ) }
