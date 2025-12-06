6df0a16c0947f739baf189b7f7564b842305ae36
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:07:16 +0100

    Update reservation.js

--- Pages/reservation.js
+++ Pages/reservation.js
@@ -1,1 +1,3 @@
+import Navbar from '../composants/Navbar'; import Footer from '../composants/Footer';
 
+export default function Reservation() { return ( <> <main style={{padding: '2rem'}}> Book a Private Chef <form style={{display:'flex', flexDirection:'column', maxWidth:'400px', margin:'auto'}}> Date: Time: Number of People: Choose a Menu: Mediterranean Asian Italian French Vegetarian Street Food Exotic Breakfast Gourmet Brunch Custom Menu Notes: <button type='submit' style={{marginTop:'1rem'}}>Send Reservation </> ) }
