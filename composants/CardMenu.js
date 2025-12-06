40f64c9748bd985890ad5eaed8f986b462490428
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:34:12 +0100

    Update CardMenu.js

--- composants/CardMenu.js
+++ composants/CardMenu.js
@@ -1,1 +1,10 @@
-export default function CardMenu({menu}){ return ( <div style={{border:'1px solid #ddd', borderRadius:'8px', padding:'1rem', width:'200px', textAlign:'center'}}> <img src={menu.image} alt={menu.title} style={{width:'100%', borderRadius:'8px'}}/> {menu.title} {menu.description} {menu.price} ) }
+export default function CardMenu({menu}){
+  return (
+    <div style={{border:'1px solid #ddd', borderRadius:'8px', padding:'1rem', width:'200px', textAlign:'center'}}>
+      <img src={menu.image} alt={menu.title} style={{width:'100%', borderRadius:'8px'}}/>
+      <h3>{menu.title}</h3>
+      <p>{menu.description}</p>
+      <p>{menu.price}</p>
+    </div>
+  )
+}
