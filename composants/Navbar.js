4ea58f2743c1ee78c8a76cdfaf34d81a79aad994
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:45:39 +0100

    Update Navbar.js

--- composants/Navbar.js
+++ composants/Navbar.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
 export default function Navbar(){
   return (
-    <nav style={{display:'flex', justifyContent:'space-between', padding:'1rem 2rem', backgroundColor:'#FF7F50', color:'white'}}>
-      <h2>ZestUp</h2>
+    <nav style={{
+      display:'flex', justifyContent:'space-between', 
+      alignItems:'center',
+      padding:'1rem 2rem',
+      backgroundColor:'#FF7F50', 
+      color:'white',
+      boxShadow:'0 4px 6px rgba(0,0,0,0.1)'
+    }}>
+      <h2 style={{fontFamily:'Montserrat', fontWeight:'700', fontSize:'1.5rem'}}>ZestUp</h2>
       <div style={{display:'flex', gap:'1rem'}}>
         <Link href='/'>Home</Link>
         <Link href='/menus'>Menus</Link>
