019c9198b706aad72dbe1875b348a1fa9f39ccd1
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-06 17:05:43 +0100

    Update index.js

--- Pages/index.js
+++ Pages/index.js
@@ -1,1 +1,3 @@
+import Navbar from '../composants/Navbar'; import Footer from '../composants/Footer'; import CarrouselImages from '../composants/CarrouselImages';
 
+export default function Home() { return ( <> <main style={{textAlign: 'center', padding: '2rem'}}> Welcome to ZestUp Explosion of flavors delivered to your door or with a private chef! <div style={{marginTop: '2rem'}}> Order Now <button style={{marginLeft:'1rem'}}>Book a Chef </> ) }
