0759fa46e9023b84c878fdde8e28995fa6b9f281
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-07 22:50:00 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,21 @@
+import '../styles/global.css';
+import Navbar from '../composants/Navbar';
+import Footer from '../composants/Footer';
+import Head from 'next/head';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>ZestUp !</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <Navbar />
+      <main style={{ minHeight: '80vh' }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
+}
