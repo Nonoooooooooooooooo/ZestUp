19576721a7f6c0d695cd1209604cb11197f1ceb1
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-07 13:10:45 +0100

    Update Hero.js

--- composants/Hero.js
+++ composants/Hero.js
@@ -1,16 +1,21 @@
 import Image from 'next/image';
 import styles from '../styles/Hero.module.css';
+import Link from 'next/link';
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.heroOverlay}>
         <h1>Explosion de saveurs à domicile</h1>
-        <p>Découvrez nos menus et commandez vos plats préférés ou réservez un chef chez vous.</p>
+        <p>Découvrez nos menus, commandez vos plats préférés ou réservez un chef chez vous.</p>
+        <Link href="/reservation">
+          <button>Réserver un chef</button>
+        </Link>
       </div>
+
       <Image
-        src="/images/hero.jpg"
-        alt="Hero Image"
+        src="/images/hero-highres.jpg" // mets une image de haute résolution ici
+        alt="Hero ZestUp"
         fill
         style={{ objectFit: 'cover', zIndex: -1 }}
         priority
@@ -18,4 +23,3 @@ export default function Hero() {
     </section>
   );
 }
-
