9dc2d69fbc3f25c4b0d292c4324b9bf6e20c2f50
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-07 13:55:09 +0100

    Update Navbar.js

--- composants/Navbar.js
+++ composants/Navbar.js
@@ -1,43 +1,40 @@
-import Link from 'next/link';
 import { useState } from 'react';
+import Link from 'next/link';
+import { FiMenu } from 'react-icons/fi';
 import styles from '../styles/Navbar.module.css';
-import { FiMenu, FiX } from "react-icons/fi";
 
 export default function Navbar() {
-  const [mobileOpen, setMobileOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <nav className={styles.navbar}>
-
-      {/* LOGO */}
       <div className={styles.logo}>
-        Zest<span className={styles.logoAccent}>Up</span>
+        <Link href="/">
+          <span className={styles.logoAccent}>ZestUp</span>
+        </Link>
       </div>
 
-      {/* MENU DESKTOP */}
       <div className={styles.menu}>
-        <Link href="/" className={styles.navLink}>Accueil</Link>
-        <Link href="/menus" className={styles.navLink}>Menus</Link>
-        <Link href="/reservation" className={styles.navLink}>Réservation</Link>
-        <Link href="/commande" className={styles.navLink}>Commande</Link>
+        <Link className={styles.navLink} href="/">Accueil</Link>
+        <Link className={styles.navLink} href="/menus">Menus</Link>
+        <Link className={styles.navLink} href="/commande">Commander</Link>
+        <Link className={styles.navLink} href="/reservation">Réservation</Link>
       </div>
 
-      {/* ICON MENU MOBILE */}
-      <div
-        className={styles.menuMobileIcon}
-        onClick={() => setMobileOpen(!mobileOpen)}
-      >
-        {mobileOpen ? <FiX /> : <FiMenu />}
+      {/* Mobile menu icon */}
+      <div className={styles.menuMobileIcon} onClick={toggleMenu}>
+        <FiMenu />
       </div>
 
-      {/* MENU MOBILE */}
-      <div className={`${styles.menuMobile} ${mobileOpen ? styles.open : ''}`}>
-        <Link href="/" className={styles.navLink}>Accueil</Link>
-        <Link href="/menus" className={styles.navLink}>Menus</Link>
-        <Link href="/reservation" className={styles.navLink}>Réservation</Link>
-        <Link href="/commande" className={styles.navLink}>Commande</Link>
+      {/* Mobile menu */}
+      <div className={`${styles.menuMobile} ${menuOpen ? 'open' : ''}`}>
+        <Link className={styles.navLink} href="/" onClick={toggleMenu}>Accueil</Link>
+        <Link className={styles.navLink} href="/menus" onClick={toggleMenu}>Menus</Link>
+        <Link className={styles.navLink} href="/commande" onClick={toggleMenu}>Commander</Link>
+        <Link className={styles.navLink} href="/reservation" onClick={toggleMenu}>Réservation</Link>
       </div>
-
     </nav>
   );
 }
