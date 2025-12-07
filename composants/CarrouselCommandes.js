cb41807c7169a1f8dc0127207a8c3f96684e411a
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-07 12:53:28 +0100

    Update CarrouselCommandes.js

--- composants/CarrouselCommandes.js
+++ composants/CarrouselCommandes.js
@@ -1,5 +1,3 @@
-import styles from '../styles/CarrouselMenus.module.css'; // on réutilise le même style
-
 const commandes = [
   { name: "Plat 1", image: "/images/plat1.jpg" },
   { name: "Plat 2", image: "/images/plat2.jpg" },
@@ -7,15 +5,29 @@ const commandes = [
 ];
 
 export default function CarrouselCommandes() {
+  const carouselRef = useRef(null);
+
+  const scrollLeft = () => carouselRef.current.scrollBy({ left: -300, behavior: "smooth" });
+  const scrollRight = () => carouselRef.current.scrollBy({ left: 300, behavior: "smooth" });
+
   return (
-    <section className={styles.carrousel}>
-      {commandes.map((plat, i) => (
-        <div key={i} className={styles.menuCard}>
-          <img src={plat.image} alt={plat.name} />
-          <h3>{plat.name}</h3>
-          <button>Commander</button>
-        </div>
-      ))}
-    </section>
+    <div className={styles.carrouselContainer}>
+      <button className={`${styles.arrowButton} ${styles.arrowLeft}`} onClick={scrollLeft}>
+        <FiChevronLeft />
+      </button>
+      <div ref={carouselRef} className={styles.carrousel}>
+        {commandes.map((plat, i) => (
+          <div key={i} className={styles.menuCard}>
+            <img src={plat.image} alt={plat.name} />
+            <h3>{plat.name}</h3>
+            <button>Commander</button>
+          </div>
+        ))}
+      </div>
+      <button className={`${styles.arrowButton} ${styles.arrowRight}`} onClick={scrollRight}>
+        <FiChevronRight />
+      </button>
+    </div>
   );
 }
+
