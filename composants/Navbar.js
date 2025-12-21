86f4dc05a1352ee0af167180bb3c8fc776888abd
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-21 18:07:59 +0100

    Update Navbar.js

--- composants/Navbar.js
+++ composants/Navbar.js
@@ -1,18 +1,14 @@
-import { useState } from 'react';
-import Link from 'next/link';
-import { FiMenu } from 'react-icons/fi';
-import { useSession, signOut } from 'next-auth/react';
-import Image from 'next/image';
-import styles from '../styles/Navbar.module.css';
+import { useState } from "react";
+import Link from "next/link";
+import { FiMenu } from "react-icons/fi";
+import { useSession, signOut } from "next-auth/react";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
   const { data: session } = useSession();
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
-
-  // URL de la photo par défaut si pas de photo
-  const defaultAvatar = "/images/avatar-default.png";
 
   return (
     <nav className={styles.navbar}>
@@ -23,53 +19,41 @@ export default function Navbar() {
       </div>
 
       <div className={styles.menu}>
-        <Link className={styles.navLink} href="/">Accueil</Link>
-        <Link className={styles.navLink} href="/menus">Menus</Link>
-        <Link className={styles.navLink} href="/commande">Commander</Link>
-        <Link className={styles.navLink} href="/reservation">Réservation</Link>
-
-        <div className={styles.userSection}>
-          {session ? (
-            <Link href="/profile" className={styles.userButton}>
-              <Image
-                src={session.user.image || defaultAvatar}
-                alt="Avatar"
-                width={32}
-                height={32}
-                className={styles.userAvatar}
-              />
-              {session.user.name}
-            </Link>
-          ) : (
-            <Link className={styles.navButton} href="/auth/signin">Connexion</Link>
-          )}
-        </div>
+        <Link href="/">Accueil</Link>
+        <Link href="/menus">Menus</Link>
+        <Link href="/commande">Commander</Link>
+        <Link href="/reservation">Réservation</Link>
       </div>
 
-      {/* Menu mobile */}
+      <div className={styles.menuRight}>
+        {session?.user ? (
+          <div className={styles.profileButton}>
+            <Link href="/profile">
+              <img src={session.user.image || "/images/avatar.png"} alt="Avatar" className={styles.avatar} />
+              {session.user.name}
+            </Link>
+            <button onClick={() => signOut()}>Déconnexion</button>
+          </div>
+        ) : (
+          <Link href="/auth/signin">
+            <button className={styles.connexionButton}>Connexion</button>
+          </Link>
+        )}
+      </div>
+
       <div className={styles.menuMobileIcon} onClick={toggleMenu}>
         <FiMenu />
       </div>
 
-      <div className={`${styles.menuMobile} ${menuOpen ? 'open' : ''}`}>
-        <Link className={styles.navLink} href="/" onClick={toggleMenu}>Accueil</Link>
-        <Link className={styles.navLink} href="/menus" onClick={toggleMenu}>Menus</Link>
-        <Link className={styles.navLink} href="/commande" onClick={toggleMenu}>Commander</Link>
-        <Link className={styles.navLink} href="/reservation" onClick={toggleMenu}>Réservation</Link>
-
-        {session ? (
-          <Link href="/profile" className={styles.userButton} onClick={toggleMenu}>
-            <Image
-              src={session.user.image || defaultAvatar}
-              alt="Avatar"
-              width={32}
-              height={32}
-              className={styles.userAvatar}
-            />
-            {session.user.name}
-          </Link>
+      <div className={`${styles.menuMobile} ${menuOpen ? "open" : ""}`}>
+        <Link href="/" onClick={toggleMenu}>Accueil</Link>
+        <Link href="/menus" onClick={toggleMenu}>Menus</Link>
+        <Link href="/commande" onClick={toggleMenu}>Commander</Link>
+        <Link href="/reservation" onClick={toggleMenu}>Réservation</Link>
+        {session?.user ? (
+          <Link href="/profile" onClick={toggleMenu}>Profil</Link>
         ) : (
-          <Link className={styles.navButton} href="/auth/signin" onClick={toggleMenu}>Connexion</Link>
+          <Link href="/auth/signin" onClick={toggleMenu}>Connexion</Link>
         )}
       </div>
     </nav>
