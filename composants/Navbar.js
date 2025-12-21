e30a09e5db920541e076ebe4ff6b6f58770f73d5
Nonoooooooooooooooo <leseigneurnolann@outlook.fr>
2025-12-21 16:43:38 +0100

    Refactor Navbar to use Image component for avatars

--- composants/Navbar.js
+++ composants/Navbar.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { FiMenu } from 'react-icons/fi';
 import { useSession, signOut } from 'next-auth/react';
+import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
@@ -10,53 +11,65 @@ export default function Navbar() {
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
+  // URL de la photo par défaut si pas de photo
+  const defaultAvatar = "/images/avatar-default.png";
+
   return (
     <nav className={styles.navbar}>
-      {/* Logo réel */}
       <div className={styles.logo}>
         <Link href="/">
           <img src="/images/logo.png" alt="ZestUp Logo" />
         </Link>
       </div>
 
-      {/* Menu desktop */}
       <div className={styles.menu}>
         <Link className={styles.navLink} href="/">Accueil</Link>
         <Link className={styles.navLink} href="/menus">Menus</Link>
         <Link className={styles.navLink} href="/commande">Commander</Link>
         <Link className={styles.navLink} href="/reservation">Réservation</Link>
-        {session ? (
-          <>
-            <span className={styles.navUser}>Bonjour {session.user.name}</span>
-            <button className={`${styles.navButton} ${styles.navLogin}`} onClick={() => signOut({ callbackUrl: "/" })}>
-              Déconnexion
-            </button>
-          </>
-        ) : (
-          <Link className={`${styles.navLink} ${styles.navLogin}`} href="/auth/signin">Connexion</Link>
-        )}
+
+        <div className={styles.userSection}>
+          {session ? (
+            <Link href="/profile" className={styles.userButton}>
+              <Image
+                src={session.user.image || defaultAvatar}
+                alt="Avatar"
+                width={32}
+                height={32}
+                className={styles.userAvatar}
+              />
+              {session.user.name}
+            </Link>
+          ) : (
+            <Link className={styles.navButton} href="/auth/signin">Connexion</Link>
+          )}
+        </div>
       </div>
 
-      {/* Icône menu mobile */}
+      {/* Menu mobile */}
       <div className={styles.menuMobileIcon} onClick={toggleMenu}>
         <FiMenu />
       </div>
 
-      {/* Menu mobile */}
       <div className={`${styles.menuMobile} ${menuOpen ? 'open' : ''}`}>
         <Link className={styles.navLink} href="/" onClick={toggleMenu}>Accueil</Link>
         <Link className={styles.navLink} href="/menus" onClick={toggleMenu}>Menus</Link>
         <Link className={styles.navLink} href="/commande" onClick={toggleMenu}>Commander</Link>
         <Link className={styles.navLink} href="/reservation" onClick={toggleMenu}>Réservation</Link>
+
         {session ? (
-          <>
-            <span className={styles.navUser}>Bonjour {session.user.name}</span>
-            <button className={`${styles.navButton} ${styles.navLogin}`} onClick={() => { signOut({ callbackUrl: "/" }); toggleMenu(); }}>
-              Déconnexion
-            </button>
-          </>
+          <Link href="/profile" className={styles.userButton} onClick={toggleMenu}>
+            <Image
+              src={session.user.image || defaultAvatar}
+              alt="Avatar"
+              width={32}
+              height={32}
+              className={styles.userAvatar}
+            />
+            {session.user.name}
+          </Link>
         ) : (
-          <Link className={`${styles.navLink} ${styles.navLogin}`} href="/auth/signin" onClick={toggleMenu}>Connexion</Link>
+          <Link className={styles.navButton} href="/auth/signin" onClick={toggleMenu}>Connexion</Link>
         )}
       </div>
     </nav>
